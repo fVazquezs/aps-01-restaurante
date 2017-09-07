f5e3900c853c4b16ea35e07964fb3c36bfa3294f
Fernando Vázquez Silva <fernando.vz00@gmail.com>
2017-09-07 13:51:28 -0300

    Atualiazação da classe restaurante, adicionado o metodo mostraCardapio

--- src/Restaurante.java
+++ src/Restaurante.java
@@ -1,4 +1,12 @@
-
 public class Restaurante {
-	private Pratos prato1 = Pratos()
+	
+	private Pratos prato1;
+	private Pratos prato2;
+	private Pratos prato3;
+	
+	private void mostrarCardapio(){
+		System.out.println(prato1.getNome() + prato1.getPreço() + "\n" + prato1.getDescricao());
+		System.out.println(prato2.getNome() + prato2.getPreço() + "\n" + prato2.getDescricao());
+		System.out.println(prato3.getNome() + prato3.getPreço() + "\n" + prato3.getDescricao());
+	}
 }
